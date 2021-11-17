e524e0b4235161ea3cc876f300023bf6de7c598f
soellm <93282131+soellm@users.noreply.github.com>
2021-11-18 01:32:54 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+const countEl = document.getElementById('count');
+
+updateVisitCount();
+
+function updateVisitCount() {
+	fetch('https://api.countapi.xyz/update/suelm.com/slmole/?amount=1')
+	.then(res => res.json())
+	.then(res => {
+		countEl.innerHTML = res.value;
+	})
+
+}
